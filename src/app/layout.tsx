70ee703b4e0201ea2af2b88b4159cf43f289c1c5
Chinito <clark.wayne023@gmail.com>
2024-03-05 09:15:57 +0800

    Added Analytics Component  for Vercel's Web Traffic

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import Navbar from '@/components/Navbar'
 import Footer from '@/components/Footer'
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -24,6 +25,7 @@ export default function RootLayout({
       <body className={`${inter.className} min-w-[350px]`}>
         <Navbar />
         {children}
+        <Analytics />
         <Footer />
       </body>
     </html>
